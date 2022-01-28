be59301b5ae9a326d7544f476b655b2a21d9cdf3
fmcshan <fmcshan@wpi.edu>
2022-01-28 18:42:04 -0500

    Refactor code

--- src/src/main.java
+++ src/src/main.java
@@ -6,4 +6,22 @@ public class main {
         // We could potentially build graph in here.
     }
 
+    public String[][] loadTerrain(String fileName) {
+        List<String> lines;
+
+        try{
+            lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+        }
+        catch(Exception e){
+
+        }
+        String[][] fileArray = new String[lines.size()][];
+
+        for(int i =0; i<lines.size(); i++){
+            fileArray[i] = lines.get(i).split("\t"); //tab-separated
+        }
+
+        return fileArray;
+    }
+
 }
