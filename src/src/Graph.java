0864cb8e8b6c9baed5ba684419381c9c85ce45c8
varneycole00 <61516468+varneycole00@users.noreply.github.com>
2022-01-28 22:26:23 -0500

    yee

---
+++ src/src/Graph.java
@@ -0,0 +1,7 @@
+import java.util.Map;
+import java.util.Set;
+
+public class Graph {
+
+
+}
---
+++ src/src/GraphNode.java
@@ -0,0 +1,9 @@
+import org.w3c.dom.Node;
+
+public class GraphNode implements Comparable<Node> {
+
+    @Override
+    public int compareTo(Node o) {
+        return 0;
+    }
+}
