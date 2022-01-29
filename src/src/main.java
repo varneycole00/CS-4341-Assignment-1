f604621f21927552e715a87b089a617e63ff66b7
varneycole00 <61516468+varneycole00@users.noreply.github.com>
2022-01-28 23:01:29 -0500

    5

--- src/src/main.java
+++ src/src/main.java
@@ -1,2 +1,9 @@
 public class main {
+
+    public static void main(String[] args) {
+        // Main code block initiating execution
+
+        // We could potentially build graph in here.
+    }
+
 }
