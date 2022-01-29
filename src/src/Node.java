e17e452521184fafa7232204752a87303e3af72b
varneycole00 <61516468+varneycole00@users.noreply.github.com>
2022-01-28 22:56:09 -0500

    Node, Edge class with v1 a*, needs modification to use case

---
+++ src/src/Node.java
@@ -0,0 +1,112 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Node implements Comparable<Node> {
+
+    private static int idCounter = 0;
+    public int id;
+
+    public Node parent = null;
+    public List<Edge> neighbors;
+
+    public double f = Double.MAX_VALUE;
+    public double g = Double.MAX_VALUE;
+
+    public double h;
+
+    Node(double h) {
+        this.h = h;
+        this.id = idCounter++; // We may want to implement a different ID system
+        this.neighbors = new ArrayList<>();
+    }
+
+    @Override
+    public int compareTo(Node n) {
+        return Double.compare(this.f, n.f);
+    }
+
+    public static class Edge {
+        Edge(int weight, Node node) {
+            this.weight = weight;
+            this.node = node;
+        }
+        public int weight;
+        public Node node;
+    }
+
+    public void addBranch(int weight, Node node) {
+        Edge newEdge = new Edge(weight, node);
+        neighbors.add(newEdge);
+    }
+
+    public double calculateHeuristic(Node target) {
+        return this.h;
+    }
+
+    public static Node aStar(Node start, Node target){
+        PriorityQueue<Node> closedList = new PriorityQueue<>();
+        PriorityQueue<Node> openList = new PriorityQueue<>();
+
+        start.f = start.g + start.calculateHeuristic(target);
+        openList.add(start);
+
+        while(!openList.isEmpty()){
+            Node n = openList.peek();
+            if(n == target){
+                return n;
+            }
+
+            for(Node.Edge edge : n.neighbors){
+                Node m = edge.node;
+                double totalWeight = n.g + edge.weight;
+
+                if(!openList.contains(m) && !closedList.contains(m)){
+                    m.parent = n;
+                    m.g = totalWeight;
+                    m.f = m.g + m.calculateHeuristic(target);
+                    openList.add(m);
+                } else {
+                    if(totalWeight < m.g){
+                        m.parent = n;
+                        m.g = totalWeight;
+                        m.f = m.g + m.calculateHeuristic(target);
+
+                        if(closedList.contains(m)){
+                            closedList.remove(m);
+                            openList.add(m);
+                        }
+                    }
+                }
+            }
+
+            openList.remove(n);
+            closedList.add(n);
+        }
+        return null;
+    }
+
+    public static void printPath(Node target){
+        Node n = target;
+
+        if(n==null)
+            return;
+
+        List<Integer> ids = new ArrayList<>();
+
+        while(n.parent != null){
+            ids.add(n.id);
+            n = n.parent;
+        }
+        ids.add(n.id);
+        Collections.reverse(ids);
+
+        for(int id : ids){
+            System.out.print(id + " ");
+        }
+        System.out.println("");
+    }
+
+
+}
