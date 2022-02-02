f78abaa07ca516a28e8d84e4931fea26c375b2f5
Jack353 <jvc353123@gmail.com>
2022-02-02 14:09:53 -0500

    timer and bigger board10

--- src/main.java
+++ src/main.java
@@ -1,6 +1,8 @@
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -19,13 +21,18 @@ public class main {
         System.out.println("input heuristic (integer 1-6)");
         int heuristic = Integer.parseInt(in.nextLine());
 
+        long startTime = System.currentTimeMillis();
+
         try {
             GraphUtils.makeGraph(file, heuristic);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        // We could potentially build graph in here.
 
+        long endTime = System.currentTimeMillis();
+
+        NumberFormat formatter = new DecimalFormat("#0.00000");
+        System.out.print("Execution time is " + formatter.format((endTime - startTime) / 1000d) + " seconds");
 
     }
 
